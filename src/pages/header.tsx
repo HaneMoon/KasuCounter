547d8808a214cd16687a65f0e5d6b3adf058143a
稲森かなでた <zy56097@gmail.com>
2025-06-04 10:45:40 +0900

    ログイン部css追加

--- src/pages/header.tsx
+++ src/pages/header.tsx
@@ -2,34 +2,35 @@
 // import styles from "./header.scss"
 import { signInWithPopup, signOut } from "firebase/auth"
 import { provider } from "../firebase"
-import {type Auth , type User} from "firebase/auth"
+import { type Auth, type User } from "firebase/auth"
+import styles from "./header.module.scss"
 
-interface HeaderProps{
-auth:Auth
-user:User | null |undefined
+interface HeaderProps {
+    auth: Auth
+    user: User | null | undefined
 
 }
 
-export default function Header({ auth,user}:HeaderProps) {
+export default function Header({ auth, user }: HeaderProps) {
 
     return (
         <>
-            <h1>位置情報をとるサイト
-                <h2 onClick={()=>{
-                    if(user){
-                        signOut(auth)
-                    }else{
-                    signInWithPopup(auth,provider)
-                    }
-                }}>
-                    {user?"ログアウト":"ログイン"}
-                </h2>
-            </h1>
+            <div className={styles.header}>
+                <h1 className={styles.title}>位置情報をとるサイト <span>
+                    <h2 onClick={() => {
+                        if (user) {
+                            signOut(auth)
+                        } else {
+                            signInWithPopup(auth, provider)
+                        }
+                    }}
+                        className={styles.login}>
+                        {user ? "ログアウト" : "ログイン"}
+                    </h2>
+                </span> </h1>
+            </div>
         </>
     )
 }
 
-// const userLogin=()=>{
 
-    
-// }
