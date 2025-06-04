2d14dcae5a242ee427371619ce5d1587f2c25f7d
稲森かなでた <zy56097@gmail.com>
2025-06-04 10:53:56 +0900

    styles.scss追加

--- src/components/location.tsx
+++ src/components/location.tsx
@@ -4,7 +4,8 @@ import Button from "react-bootstrap/Button"
 export default function Location() {
     const [location, setLocation] = useState({ lat: 0, lng: 0 })
     return (
-        <>        <h1>位置情報</h1>
+        <>
+            <h1>位置情報</h1>
             <div>
                 <div>緯度: {location.lat}</div>
                 <div>経度: {location.lng}</div>
